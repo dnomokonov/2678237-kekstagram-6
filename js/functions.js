834883c9b94e5ecf8d09694615e3f3a1c6ab15b6
dnomokonov <zmnomokon715@gmail.com>
2025-11-19 19:50:13 +0700

    Add base function

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+function checkStrLen(str, len) {
+  return str.length <= len;
+}
+
+function isPalindrome(str) {
+  const lowerStr = str.toLowerCase().replaceAll(' ', '');
+  return lowerStr === lowerStr.split('').reverse().join('');
+}
+
+function extractDigits(str) {
+  let result = '';
+  str = str.toString();
+
+  for (let char of str) {
+    if (!isNaN(parseInt(char, 10))) {
+      result += char;
+    }
+  }
+  result = parseInt(result, 10) ? result : NaN;
+  return result;
+}
