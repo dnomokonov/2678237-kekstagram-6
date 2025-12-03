61ae6819512bfb41dfbff4d843bb399ce6a81976
dnomokonov <zmnomokon715@gmail.com>
2025-12-03 19:39:20 +0700

    Add render thumbnails

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import { generatePosts } from './data.js';
-import {COUNT_POSTS} from './constants';
+import { COUNT_POSTS } from './constants.js';
+import { renderThumbnails } from './renderThumbnails.js';
 
-generatePosts(COUNT_POSTS);
+const posts = generatePosts(COUNT_POSTS);
+
+renderThumbnails(posts);
